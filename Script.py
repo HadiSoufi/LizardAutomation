7092a651c241ce67d7dd34c26f0747945d8c4795
Hadi Soufi <soufihadi@yahoo.com>
2023-02-08 12:59:52 -0500

    Improve modularity

--- Script.py
+++ Script.py
@@ -10,81 +10,73 @@ from enum import Enum
 from datetime import datetime
 from datetime import timedelta
 
-# Simple enum for code clarity
-class DayState(Enum):
-    night = 1
-    sunrise = 2
-    day = 3
-    sunset = 4
-
 # CONFIG
-sun_data = Sun(35.227085, -80.843124)   # Use your latitude/longitude here
+latitude = 35.227085                    # Timezone latitude
+longitutde = -80.843124                 # Timezone longitude
 change_hours = 2                        # Number of hours for sunrise/sunset
-ping_delay = 1                          # Amount of time to wait between running each loop. Higher values lead to increased smoothness during sunrise/sunset
-strip = SmartStrip("192.168.1.160")     # Reference to actual strip
-dimmer = SmartDimmer("192.168.1.161")   # Reference to actual dimmer
+ping_delay = 1                          # Amount of time in seconds to wait between running each loop. Higher values lead to increased smoothness during sunrise/sunset
+strip = SmartStrip("192.168.1.160")     # Strip object
+dimmer = SmartDimmer("192.168.1.161")   # Dimmer object
 
-# See bottom line
+fade_time = timedelta(hours = change_hours)     # Convert change_hours to a datetime object
+sun_data = Sun(latitude, longitude)             # Convert change_hours to a datetime object
+
+# Turn on all switches
+async def turn_on():
+    await dimmer.turn_on()
+    await dimmer.set_brightness(100)
+    await uvb.turn_on()
+    await plant.turn_on()
+
+# Set brightness on dimmer switches, turn others on
+async def set_brightness(brightness)
+    await dimmer.turn_on()
+    await dimmer.set_brightness(brightness)
+    await uvb.turn_on()
+    await plant.turn_on()
+    
+# Turn off all switches    
+async def turn_off():
+    await dimmer.turn_off()
+    await uvb.turn_off()
+    await plant.turn_off()
+
+# Update all smart devices
+async def update():
+    await strip.update()
+    await dimmer.update()
+    
+# Main event loop
 async def main():
     while True:
         try:
             # Must be called before any other API calls
-            await strip.update()
-            await dimmer.update()
+            asyncio.run(update())
             
             # Gather individual plugs
             heat = strip.children[0]
             uvb = strip.children[1]
             plant = strip.children[2]
 
-            # Get time of day and sunrise/set times in local timezone
+            # Get time of day, sunrise, and sunset in local timezone
             sunrise = sun_data.get_sunrise_time().astimezone()
             sunset = sun_data.get_sunset_time().astimezone()
             now = datetime.now().astimezone()
 
             # Calculate the state of day
             if now > sunrise and now < sunset:
-                if now < sunrise + timedelta(hours=change_hours):
-                    day_state = DayState.sunrise
-                elif now > sunset - timedelta(hours=change_hours):
-                    day_state = DayState.sunset
+                # Sunrise
+                if now < sunrise + fade_time:
+                    asyncio.run(set_brightness(int ((sunset - now) / fade_time)))
+                # Sunset
+                elif now > sunset - fade_time:
+                    asyncio.run(set_brightness(int ((sunset - now) / fade_time)))
+                # Day
                 else:
-                    day_state = DayState.day
+                    asyncio.run(turn_on())
+            # Night
             else:
-                day_state = DayState.night
-
-            # Turn everything off at night
-            if day_state == DayState.night:
-                await dimmer.turn_off()
-                await uvb.turn_off()
-                await plant.turn_off()
-
-            # Turn everything on during the day
-            elif day_state == DayState.day:
-                await dimmer.turn_on()
-                await dimmer.set_brightness(100)
-                await uvb.turn_on()
-                await plant.turn_on()
-
-            # Slowly dim the bulbs at sunrise
-            elif day_state == DayState.sunrise:
-                await dimmer.turn_on()
-                await uvb.turn_on()
-                await plant.turn_on()
-
-                diff = now - sunrise
-                brightness = int (diff / timedelta(hours=change_hours))
-                await dimmer.set_brightness(brightness)
-
-            # Slowly dim the bulbs at sunset
-            elif day_state == DayState.sunset:
-                await dimmer.turn_on()
-                await uvb.turn_on()
-                await plant.turn_on()
-
-                diff = sunset - now
-                brightness = int (diff / timedelta(hours=change_hours))
-                await dimmer.set_brightness(brightness)
+                asyncio.run(turn_off())
         
         # Exception handling. Most commonly caused by hardware or network issue.
         except asyncio.CancelledError as err:
@@ -93,7 +85,7 @@ async def main():
             print(err)   
         finally:
             await asyncio.sleep(ping_delay)
-
+            
 # if in Jupyter, "await main" will be fine
 if __name__ == "__main__":
     asyncio.run(main())
