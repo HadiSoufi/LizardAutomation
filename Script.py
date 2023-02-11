9a5226d4f8038173f0c65e5abcb8b62e1e675a0e
Hadi Soufi <soufihadi@yahoo.com>
2023-02-11 17:14:43 -0500

    Add dimmers to script.py

--- Script.py
+++ Script.py
@@ -11,40 +11,74 @@ from datetime import datetime
 from datetime import timedelta
 
 # CONFIG
-latitude = 35.227085                    # Timezone latitude
-longitude = -80.843124                  # Timezone longitude
-change_hours = 2                        # Number of hours for sunrise/sunset
-ping_delay = 1                          # Amount of time in seconds to wait between running each loop. Higher values lead to increased smoothness during sunrise/sunset
-strip = SmartStrip("192.168.1.160")     # Strip object
-dimmer = SmartDimmer("192.168.1.161")   # Dimmer object
+latitude = 35.227085                            # Timezone latitude
+longitude = -80.843124                          # Timezone longitude
+change_hours = 2                                # Number of hours for sunrise/sunset
+ping_delay = 1                                  # Amount of time in seconds to wait between running each loop. Higher values lead to increased smoothness during sunrise/sunset
+strip = SmartStrip("192.168.1.160")             # Smart strip
+dimmers = [[SmartDimmer("192.168.1.161"), 1],   # Dimmers to sync to day/night. 
+           [SmartDimmer("192.168.1.164"), 3]    # First number is dimmer ip, second is position on strip.
+heat = SmartDimmer("192.168.1.165")             # Heat dimmer is kept separate for temperature control.
 
 fade_time = timedelta(hours = change_hours)     # Convert change_hours to a datetime object
 sun_data = Sun(latitude, longitude)             # Get sunrise/sunset data from provided latitude/longitude
 
 # Turn on all switches
 async def turn_on():
-    await strip.children[0].turn_on()
-    await strip.children[1].turn_on()
-    await strip.children[2].turn_on()
-    
-    await dimmer.turn_on()
-    await dimmer.set_brightness(100)
+    for dimmer in dimmers:
+       await dimmer[0].turn_on()
+       await dimmer[0].set_brightness(100)
 
-# Set brightness on dimmer switches, turn others on
+# Set brightness on all switches
 async def set_brightness(brightness):
-    await strip.children[0].turn_on()
-    await strip.children[1].turn_on()
-    await strip.children[2].turn_on()
-    
-    await dimmer.turn_on()
-    await dimmer.set_brightness(brightness)
+    for dimmer in dimmers:
+       await dimmer[0].turn_on()
+       await dimmer[0].set_brightness(brightness)
     
 # Turn off all switches    
 async def turn_off():
-    # await strip.children[0].turn_off()
-    await strip.children[1].turn_off()
-    await strip.children[2].turn_off()
-    await dimmer.turn_off()
+    for dimmer in dimmers:
+       await dimmer[0].turn_off()
+    await heat.turn_off()
+
+# Validate dimmers
+async def update_dimmers():
+    for dimmer_data in dimmers:
+        i = dimmer_data[1] - 1
+        
+        try:
+            await dimmer_data[0].update()
+        except asyncio.CancelledError as err:
+            print("Operation cancelled.")
+            return False
+        except kasa.exceptions.SmartDeviceException as err:
+            if strip.children[i].is_on:
+                print("Error connecting to Smart Dimmer: " + err)
+                print("Make sure the device is connected to the network, or check the IP.")
+            else:
+                print("Error connecting to Smart Dimmer. Attempting to resolve automatically.")
+                await strip.children[i].turn_on()
+            return False
+        else:
+            return True
+    
+# Validate dimmers
+async def update_heat():
+    try:
+        await heat.update()
+    except asyncio.CancelledError as err:
+        print("Operation cancelled.")
+        return False
+    except kasa.exceptions.SmartDeviceException as err:
+        if strip.children[1].is_on:
+            print("Error connecting to Smart Dimmer: " + err)
+            print("Make sure the device is connected to the network, or check the IP.")
+        else:
+            print("Error connecting to Smart Dimmer. Attempting to resolve automatically.")
+            await strip.children[1].turn_on()
+        return False
+    else:
+        return True
     
 # Main event loop
 async def main():
@@ -68,19 +102,10 @@ async def main():
             print("Make sure the device is connected to the network, or check the IP.")
             continue
         
-        # Validate dimmer
-        try:
-            await dimmer.update()
-        except asyncio.CancelledError as err:
-            print("Operation cancelled.")
+        # Validate dimmers
+        if !update_dimmers():
             continue
-        except kasa.exceptions.SmartDeviceException as err:
-            if strip.children[0].is_on:
-                print("Error connecting to Smart Dimmer: " + err)
-                print("Make sure the device is connected to the network, or check the IP.")
-            else:
-                print("Error connecting to Smart Dimmer. Attempting to resolve automatically.")
-                await strip.children[0].turn_on()
+        if !update_heat():
             continue
             
         # Update lights
