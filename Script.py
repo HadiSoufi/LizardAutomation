049f215dd891815208e0d0ad41f5619114252b70
HadiSoufi <soufihadi@yahoo.com>
2023-10-07 18:06:43 -0400

    at last, grug find cut point- defeat spirit complexity demon. wise grug know, will return if not vigilant

--- Script.py
+++ Script.py
@@ -18,7 +18,7 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 # Load from config.ini
-ping_delay = int(config.get('Core', 'Update time'))
+update_delay = int(config.get('Core', 'Update time'))
 dimmer_ips = config.get('Core', 'Dimmers').split(', ')
 
 latitude = float(config.get('Timezone', 'Latitude'))
@@ -34,15 +34,15 @@ dimmers = [SmartDimmer(ip) for ip in dimmer_ips]
 # 192.168.1.161, 192.168.1.188
 
 # Timezone
-fade_time = timedelta(hours=change_hours)               # Convert change_hours to a datetime object
-sun_data = Sun(latitude, longitude)                     # Get sunrise/sunset data from provided latitude/longitude
+fade_time = timedelta(hours=change_hours)                               # Convert change_hours to a datetime object
+sun_data = Sun(latitude, longitude)                                     # Sunrise/sunset from latitude/longitude
 tz_name = TimezoneFinder().timezone_at(lng=longitude, lat=latitude)     # Timezone calculated from provided lat/long
-tz = ZoneInfo(tz_name)                                  # Timezone object from tz_name
+tz = ZoneInfo(tz_name)                                                  # Timezone object from tz_name
 
 # SMS
-CARRIERS = {                                            # List of mobile carrier gateway addresses.
-    "att": "mms.att.net",                               # If you don't see yours here, check:
-    "tmobile": "tmomail.net",                           # https://en.wikipedia.org/wiki/SMS_gateway#Email_clients
+CARRIERS = {                                                            # List of mobile carrier gateway addresses.
+    "att": "mms.att.net",                                               # If you don't see yours here, check:
+    "tmobile": "tmomail.net",                                 # https://en.wikipedia.org/wiki/SMS_gateway#Email_clients
     "verizon": "vtext.com",
     "sprint": "messaging.sprintpcs.com",
     "boost": "smsmyboostmobile.com",
@@ -50,39 +50,65 @@ CARRIERS = {                                            # List of mobile carrier
     "uscellular": "email.uscc.net"
 }
 
-carrier = ''.join(c for c in carrier if c.isalnum()).lower()
-recipient = phone_number + "@" + CARRIERS[carrier]
-text_email = "lizardautomator@gmail.com"                # Feel free to replace with your own email
-text_password = "pven okhn kbmv wvmh"                   # App password for gmail- https://tinyurl.com/3svawyjn
-server = smtplib.SMTP("smtp.gmail.com", 587)  # Replace this with the correct email client if not using gmail
+carrier = ''.join(c for c in carrier if c.isalnum()).lower()            # Sanitize user setting
+recipient = phone_number + "@" + CARRIERS[carrier]                      # Create recipient string
+text_email = "lizardautomator@gmail.com"                                # Feel free to replace with your own email
+text_password = "pven okhn kbmv wvmh"                                   # https://tinyurl.com/3svawyjn
+server = smtplib.SMTP("smtp.gmail.com", 587)                  # Only change if not using gmail
 last_text_time = None
 
 
-# Main event loop
+# Main loop
 async def main():
     while True:
-        # Validate dimmers
-        if not await lights_update():
-            continue
-
         # Get time of day, sunrise, and sunset in local timezone
         now = datetime.now(tz)
         sunrise = sun_data.get_local_sunrise_time(now, tz) - fade_time
         sunset = sun_data.get_local_sunset_time(now, tz) + fade_time
 
-        # Calculate bulb brightness
-        brightness = bulb_brightness(now, sunrise, sunset)
+        # Calculate brightness
+        brightness = get_brightness(now, sunrise, sunset)
 
-        # Set bulb values appropriately
-        if brightness == 0:
-            await lights_off()
-        elif brightness == 100:
-            await lights_on()
+        # Update brightness
+        await set_lights_brightness(brightness)
+
+        # Delay until next update
+        await asyncio.sleep(update_delay)
+
+
+# Calculate the brightness based on time of day
+def get_brightness(now, sunrise, sunset):
+    if sunrise < now < sunset:
+        # Sunrise
+        if now < (sunrise + fade_time):
+            return int(100 * (now - sunrise) / fade_time)
+        # Sunset
+        elif now > (sunset - fade_time):
+            return int(100 * (sunset - now) / fade_time)
+        # Day
         else:
-            await lights_dim(brightness)
+            return 100
+    # Night
+    else:
+        return 0
 
-        # Delay between iterations
-        await asyncio.sleep(ping_delay)
+
+# Set brightness on all switches
+async def set_lights_brightness(brightness):
+    for i, dimmer in enumerate(dimmers):
+        try:
+            await dimmer.update()
+
+            if brightness == 0:
+                await dimmer.turn_off()
+            else:
+                await dimmer.turn_on()
+                await dimmer.set_brightness(brightness)
+        except kasa.exceptions.SmartDeviceException as err:
+            logging.warning("Lost connection with dimmer " + str(i) + ".\n\t" + err)
+            send_text("Dimmer not responding, but server is running. Dimmer unplugged, bad IP, or hardware failure.")
+        else:
+            continue
 
 
 # Texting service. Works by sending an email to your carrier's gateway address, which is then forwarded as a text.
@@ -100,102 +126,6 @@ def send_text(message):
             logging.error("Unable to send text: " + e)
     else:
         logging.warning(message)
-
-
-# Turn on all switches
-async def lights_on():
-    try:
-        for dimmer in dimmers:
-            await dimmer.turn_on()
-            await dimmer.set_brightness(100)
-    except asyncio.CancelledError:
-        print("Operation cancelled.")
-    except kasa.exceptions.SmartDeviceException as err:
-        print(err)
-
-
-# Set brightness on all switches
-async def lights_dim(brightness):
-    try:
-        for dimmer in dimmers:
-            await dimmer.turn_on()
-            await dimmer.set_brightness(brightness)
-    except asyncio.CancelledError:
-        print("Operation cancelled.")
-    except kasa.exceptions.SmartDeviceException as err:
-        print(err)
-
-
-# Turn off all switches
-async def lights_off():
-    try:
-        for dimmer in dimmers:
-            await dimmer.turn_off()
-    except asyncio.CancelledError:
-        print("Operation cancelled.")
-    except kasa.exceptions.SmartDeviceException as err:
-        print(err)
-
-
-# Must be called before doing any other operations
-async def lights_update():
-    for i, dimmer in enumerate(dimmers):
-        try:
-            await dimmer.update()
-        except asyncio.CancelledError:
-            print("Operation cancelled.")
-            return False
-        except kasa.exceptions.SmartDeviceException as err:
-            logging.warning("Lost connection with dimmer " + str(i))
-            send_text("Dimmer not responding, but server is running. Dimmer unplugged, bad IP, or hardware failure.")
-            return False
-        else:
-            return True
-
-
-# For testing the efficacy of time of day calculations
-def debug():
-    now = datetime.now(tz).replace(hour=0, minute=0, second=0, microsecond=0)
-    increment_hours = 1/60
-
-    while True:
-        # Update current time
-        now += timedelta(hours=increment_hours)
-
-        # Get time of day, sunrise, and sunset in local timezone
-        sunrise = sun_data.get_local_sunrise_time(now, tz)
-        sunset = sun_data.get_local_sunset_time(now, tz)
-
-        val = ''
-        brightness = bulb_brightness(now, sunrise, sunset)
-        if brightness == 0:
-            val = 'Night'
-        elif brightness == 100:
-            val = 'Day'
-        else:
-            if now < (sunrise + fade_time):
-                val = 'Sunrise: '
-            elif now > (sunset - fade_time):
-                val = 'Sunset: '
-            val += str(brightness)
-        print(val)
-
-
-# Calculate the bulb's brightness based on time of day
-def bulb_brightness(now, sunrise, sunset):
-    if sunrise < now < sunset:
-        # Sunrise
-        if now < (sunrise + fade_time):
-            return int(100 * (now - sunrise) / fade_time)
-        # Sunset
-        elif now > (sunset - fade_time):
-            return int(100 * (sunset - now) / fade_time)
-        # Day
-        else:
-            return 100
-    # Night
-    else:
-        return 0
 
 
 if __name__ == "__main__":
